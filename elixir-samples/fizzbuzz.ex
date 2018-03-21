9726501d8020df658634ac05fe305e6fcb80aca6
amagdas <magdasadrian@gmail.com>
2018-03-21 12:51:40 +0200

    Add dive into elixir presentation.

--- elixir-samples/fizzbuzz.ex
+++ elixir-samples/fizzbuzz.ex
@@ -1,7 +1,11 @@
 defmodule FizzBuzz do
-    def play(min, max), do: Enum.each(min..max, &play/1)
+    def play(min, max) when min < max, do: Enum.each(min..max, &play/1)
+
     def play(num) when rem(num, 15) == 0, do: IO.puts "FizzBuzz"
+
     def play(num) when rem(num, 3) == 0, do: IO.puts "Fizz"
+
     def play(num) when rem(num, 5) == 0, do: IO.puts "Buzz"
+
     def play(num), do: IO.puts num
 end
